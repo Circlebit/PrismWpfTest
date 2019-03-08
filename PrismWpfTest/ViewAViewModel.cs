b7071ce7cee2abbc1b823b4f13da9296f2e7059d
Paul Seibel <paul.seibel@mailbox.org>
2019-03-08 15:53:52 +0100

    Demo-Setup: two Textboxes with binded lables and an Add-Button

---
+++ PrismWpfTest/ViewAViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PrismWpfTest
+{
+    class ViewAViewModel : BindableBase
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { SetProperty(ref _firstName, value); }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set { SetProperty(ref _lastName, value); }
+        }
+
+        private List<Name> _names;
+        public List<Name> Names
+        {
+            get { return _names; }
+            set { SetProperty(ref _names, value); }
+        }
+
+
+
+    }
+
+    class Name
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+
+        public string Full => First + ' ' + Last;
+
+        public Name(string firstName, string lastName)
+        {
+            First = firstName;
+            Last = lastName;
+        }
+    }
+}
